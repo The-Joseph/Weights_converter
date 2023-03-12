80d38025bce8c9d79d22d0bf8639f0500bc63100
The-Joseph <josephwarren3141@gmail.com>
2023-03-12 13:52:43 +0000

    fix formula

--- converting_weights_-_health.py
+++ converting_weights_-_health.py
@@ -2,13 +2,15 @@
 weight = int(input("Weight: "))
 unit = input("(k)g or (l)bs: ")
 if unit.upper() == "K":
-    converted = weight / 0.45
-    print("Weight in Lbs: " + str(converted))
+    weight_in_kilos = weight
+    weight_in_pounds = weight_in_kilos/ 0.45
+    print("Weight in Lbs: " + str(weight_in_pounds))
 else:
-    converted = weight * 0.45
-    print("Weight in Kgs: " + str(converted))
+    weight_in_kilos = weight * 0.45
+    print("Weight in Kgs: " + str(weight_in_kilos))
 
 height = int(input("What is your height in centimeters?: "))
+height_in_meters = height / 100
+BMI = weight_in_kilos / (height_in_meters * height_in_meters)
 
-BMI = height * height // weight
 print(f"Your Body mass index is, {BMI}")
